b22a9490393424766e06ddd0a6f637bbf5318901
Mohit kumar <async.mohitt@gmail.com>
2025-02-24 01:22:39 +0530

    Implement user registration validation with express-validator

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,15 +1,30 @@
-const express  =  require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 // router.use(express.json())
 // router.use(express.urlencoded({extended:true}))
+const { body, validationResult } = require("express-validator");
+router.get("/register", (req, res) => {
+  res.render("register");
+});
 
-router.get('/register',(req,res)=>{
-    res.render('register')
-})
+router.post(
+  "/register",
+  body("email").trim().isEmail().isLength({ min: 13 }),
+  body("password").trim().isLength({ min: 5 }),
+  body("username").trim().isLength({ min: 3 }),
+  (req, res) => {
+    const error = validationResult(req);
 
-router.post('/register',(req,res)=>{
-    console.log(req.body)
-    res.send("usserr is registeredddd")
-})
-module.exports = router
+    if(!error.isEmpty()){
+        return res.status(400).json({
+            errors:error.array(),
+            message:"Invalid Data"
+        })
+    }
+
+    console.log(error);
+    res.send(error);
+  }
+);
+module.exports = router;
